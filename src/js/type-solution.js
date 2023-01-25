174d39c511a7aa06b2c111829597db35bf1b6ead
AlejandroJimenezMurciano <karimtilouni94@gmail.com>
2023-01-25 15:04:21 +0100

    Wordle Game

--- src/js/type-solution.js
+++ src/js/type-solution.js
@@ -1,25 +1,60 @@
-import { checkWinner } from "./check-winner.js";
-import { wordChosen } from "./word.js";
+import { checkWinner } from './check-winner.js';
+import { wordChosen } from './word.js';
 
 let rowCounter = 0;
 
-const typeSolution = (word, gameBoard) => {
+// const typeSolution = (word, gameBoard) => {
+//   for (let index = 0; index < word.length; index++) {
+//     let letter = word[index];
+//     let box = gameBoard.children[rowCounter].children[index];
+//     box.textContent = letter;
+//     if (letter === wordChosen[index]) {
+//       box.classList.add('letter-correct');
+//     } else if (wordChosen.includes(letter)) {
+//       box.classList.add('letter-contains');
+//     } else {
+//       box.classList.add('letter-not-included');
+//     }
+//   }
+//   rowCounter++;
+// };
+const paintLetter = (letter, position, className, gameBoard) => {
+  const element = gameBoard.children[rowCounter].children[position];
+  element.classList.add(className);
+  element.textContent = letter;
+};
 
+const typeSolution = (word, gameBoard) => {
+  let letter;
+  let box;
+  let className;
+  let wordToChange = wordChosen;
   for (let index = 0; index < word.length; index++) {
-    let letter = word[index]
-    let box =  gameBoard.children[rowCounter].children[index]
-    box.textContent = letter;
-    if(letter === wordChosen[index]){
-      box.classList.add('letter-correct')
-    } else if(wordChosen.includes(letter)){
-      box.classList.add('letter-contains')
-    }else{
-      box.classList.add('letter-not-included')
+    letter = word[index];
+    box = gameBoard.children[rowCounter].children[index];
+
+    if (letter === wordChosen[index]) {
+      className = 'letter-correct';
+      wordToChange = wordToChange.replace(letter, '-');
+      paintLetter(letter, index, className, gameBoard);
     }
-    
   }
-  rowCounter++
+
+  for (let index = 0; index < wordToChange.length; index++) {
+    letter = word[index];
+    box = gameBoard.children[rowCounter].children[index];
+    // box.textContent = letter;
+
+    if (wordToChange.includes(letter)) {
+      className = 'letter-contains';
+      wordToChange = wordToChange.replace(letter, '-');
+      console.log(wordToChange);
+      paintLetter(letter, index, className, gameBoard);
+      // box.textContent = letter;
+    }
+  }
+  rowCounter++;
 };
 
 export { typeSolution };
-export {rowCounter};
+export { rowCounter };
